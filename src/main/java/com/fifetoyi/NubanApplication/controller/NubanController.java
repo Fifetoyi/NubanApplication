9a93df57520501f3babfb83c8ed481afd67479be
Fifetoyi <fifetoyi@gmail.com>
2023-05-21 23:55:41 +0100

    Add Nuban service to separate logic from controller

--- src/main/java/com/fifetoyi/NubanApplication/controller/NubanController.java
+++ src/main/java/com/fifetoyi/NubanApplication/controller/NubanController.java
@@ -3,9 +3,8 @@ package com.fifetoyi.NubanApplication.controller;
 import com.fifetoyi.NubanApplication.model.Nuban;
 import com.fifetoyi.NubanApplication.repository.NubanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Random;
 
 @RestController
 @RequestMapping("/nuban")
@@ -20,32 +19,9 @@ public class NubanController {
 
     @PostMapping
     public Nuban createdNuban(@RequestParam("bankCode") String bankCode, @RequestParam("serial") int serial) {
-        // Generate NUBAN logic
-        String nuban = generateNuban(bankCode, serial);
 
-        // Create a new Nuban object
-        Nuban newNuban = new Nuban();
-        newNuban.setBankCode(bankCode);
-        newNuban.setSerial(serial);
-        newNuban.setNuban(nuban);
-
-        // Save the new Nuban object to the database
-        Nuban savedNuban = nubanRepository.save(newNuban);
-
-        return savedNuban;
-    }
-
-    private String generateNuban(String bankCode, int serial) {
-        Random random = new Random();
-        int randomNumber = random.nextInt(900000000) + 1000000000;
-        return String.valueOf(randomNumber);
-
-//        return bankCode + String.format("%09d", serial);
-    }
-
-    @GetMapping
-    public String sayHello() {
-        return "Hello";
+        NubanService nubanService = new NubanService(nubanRepository);
+        return nubanService.generateNuban(bankCode, serial);
     }
 
 }
---
+++ src/main/java/com/fifetoyi/NubanApplication/controller/NubanService.java
@@ -0,0 +1,36 @@
+package com.fifetoyi.NubanApplication.controller;
+
+import com.fifetoyi.NubanApplication.model.Nuban;
+import com.fifetoyi.NubanApplication.repository.NubanRepository;
+
+import java.util.Random;
+
+public class NubanService {
+
+    private final NubanRepository nubanRepository;
+
+    public NubanService(NubanRepository nubanRepository) {
+        this.nubanRepository = nubanRepository;
+    }
+
+    public Nuban generateNuban(String bankCode, int serial) {
+        // Generate NUBAN logic here
+        String generatedNuban = generateNubanLogic(bankCode, serial);
+
+        // Save Nuban to the database
+        Nuban nuban = new Nuban();
+        nuban.setBankCode(bankCode);
+        nuban.setSerial(serial);
+        nuban.setNuban(generatedNuban);
+        nubanRepository.save(nuban);
+
+        return nuban;
+    }
+
+    public String generateNubanLogic(String bankCode, int serial) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(900000000) + 1000000000;
+        return String.valueOf(randomNumber);
+    }
+
+}
