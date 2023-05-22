be2efc41ce00d7b52e20f910a6b5f06e80429b5b
Fifetoyi <fifetoyi@gmail.com>
2023-05-22 22:26:14 +0100

    Add tests

--- src/test/java/com/fifetoyi/NubanApplication/NubanApplicationTests.java
+++ src/test/java/com/fifetoyi/NubanApplication/NubanApplicationTests.java
@@ -2,11 +2,12 @@ package com.fifetoyi.NubanApplication;
 
 import com.fifetoyi.NubanApplication.model.Nuban;
 import com.fifetoyi.NubanApplication.repository.NubanRepository;
+import com.fifetoyi.NubanApplication.service.NubanService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class NubanApplicationTests {
@@ -16,22 +17,47 @@ class NubanApplicationTests {
 
 	@Test
 	public void givenGeneratedNuban_return_validParametersAndSaveToDatabase() {
-		// Create a new Nuban object
-		Nuban newNuban = new Nuban();
-		newNuban.setBankCode("ABC");
-		newNuban.setSerial(123);
-		newNuban.setNuban("ABC123");
 
-		// Save the Nuban to the database
-		Nuban savedNuban = nubanRepository.save(newNuban);
+		NubanService nubanService = new NubanService(nubanRepository);
+		String bankCode = "ABC";
+		int serial = 123;
+		Nuban resultNuban = nubanService.generateNuban(bankCode, serial);
 
-		// Retrieve the Nuban from the database
+		Nuban savedNuban = nubanRepository.save(resultNuban);
+
 		Nuban retrievedNuban = nubanRepository.findById(savedNuban.getId()).orElse(null);
 
-		// Verify that the Nuban is saved to the database
 		assertEquals(savedNuban.getBankCode(), retrievedNuban.getBankCode());
 		assertEquals(savedNuban.getSerial(), retrievedNuban.getSerial());
 		assertEquals(savedNuban.getNuban(), retrievedNuban.getNuban());
+	}
+
+	@Test
+	public void givenValidParameters_return_random10DigitNuban() {
+		NubanService nubanService = new NubanService(nubanRepository);
+		String bankCode = "ABC";
+		int serial = 123;
+		String resultNuban = nubanService.generateNubanLogic(bankCode, serial);
+
+		assertNotNull(resultNuban);
+		assertEquals(10, resultNuban.length());
+		assertTrue(resultNuban.matches("\\d{10}"));
+	}
+
+	@Test
+	public void givenValidParameters_return_differentRandomNumbersOnEachCall() {
+		NubanService nubanService = new NubanService(nubanRepository);
+		String bankCode = "ABC";
+		int serial = 123;
+
+		String resultNuban1 = nubanService.generateNubanLogic(bankCode, serial);
+		String resultNuban2 = nubanService.generateNubanLogic(bankCode, serial);
+		String resultNuban3 = nubanService.generateNubanLogic(bankCode, serial);
+
+		// Verify that each result is different
+		assertNotEquals(resultNuban1, resultNuban2);
+		assertNotEquals(resultNuban1, resultNuban3);
+		assertNotEquals(resultNuban2, resultNuban3);
 	}
 
 }
